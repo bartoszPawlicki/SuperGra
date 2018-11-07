f6a7d59acf6d6ce13545145a3869851620e7b313
Adamj500 <adamj500@interia.pl>
2018-11-07 22:56:27 +0100

    Problemy ze sterowaniem

---
+++ SuperGra/Assets/Scripts/Movement.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour {
+
+    private Vector3 movementVector;
+    private CharacterController characterController;
+    private float movementSpeed = 15;
+
+
+	void Start () {
+        characterController = GetComponent<CharacterController>();
+	}
+	
+
+	void Update () {
+
+        //movementVector.x = Input.GetAxis("Horizontal") * movementSpeed;
+        //movementVector.z = Input.GetAxis("Vertical") * movementSpeed;
+        movementVector.x = Input.GetAxis("LeftJoystickX") * movementSpeed;
+        movementVector.z = Input.GetAxis("LeftJoystickY") * movementSpeed;
+        movementVector.y = 0;
+
+        characterController.Move(movementVector * Time.deltaTime);
+	}
+}
---
+++ SuperGra/Assets/Scripts/PlayerHealth.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerHealth : MonoBehaviour {
+
+    public int startingHealth = 100;
+    public int currentHealth;
+    public Slider healthSlider;
+    public Text deathText;
+
+    bool isDead;
+    bool damaged;
+
+	void Awake () {
+        currentHealth = startingHealth;
+	}
+	
+	void Update () {
+		
+
+	}
+
+    public void TakeDamage (int amount)
+    {
+        damaged = true;
+        currentHealth -= amount;
+        healthSlider.value = currentHealth;
+
+        if(currentHealth <= 0 && !isDead)
+        {
+            Death();
+        }
+    }
+    void Death()
+    {
+        isDead = true;
+        deathText.text = "YOU DEAD SON";
+    }
+}
