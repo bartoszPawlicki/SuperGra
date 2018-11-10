72831d574ae7f31cd144a545a63f063be34d9a47
Adamj500 <adamj500@interia.pl>
2018-11-10 20:07:50 +0100

    Zapomniałem wrzucić, więc sterowanie, gracz, i zalążek życia

---
+++ SuperGra/Assets/Scripts/Movement.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour {
+
+    private Vector3 movementVector;                         //Zmienna służąca do poruszania się po wszystkich 3 osiach
+    private CharacterController characterController;        //Tworzymy obiekt klasy Character Controller, ktora zostala dodana do obiektu gracza
+    private float movementSpeed = 15;
+    private float gravity = 100;
+
+
+	void Start () {
+        characterController = GetComponent<CharacterController>();      //Pobieramy tę dodaną do obiektu klasę
+	}
+	
+
+	void Update () {
+        movementVector.x = Input.GetAxis("LeftJoystickX") * movementSpeed;      //Poruszanie się po osi X, wartość w cudzysłowie pobrana jest z -> Edit/Project Settings/Input
+        movementVector.z = Input.GetAxis("LeftJoystickY") * movementSpeed;      //Poruszanie po osi Y
+        movementVector.y = 0;
+
+        movementVector.y -= gravity * Time.deltaTime;       //Grawitacja, nie bedziemy się poruszać po osi y, bo raczej u nas skoku nie będzie,
+                                                            //ale jeśli teren będzie się obniżał, to nasza postać będzie latać, a tego nie chcemy
+
+        characterController.Move(movementVector * Time.deltaTime);      //.Move jest wbudowane, ta funkcja służy czysto do poruszania się
+	}
+}
