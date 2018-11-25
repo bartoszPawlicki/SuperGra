e3cb73a05485a7d6302154ed9b4125af26b1c826
precel120 <darkuso@o2.pl>
2018-11-25 16:08:41 +0100

    doors in develop

---
+++ SuperGra/Assets/Scripts/Open.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.Networking;
+
+public class Open : MonoBehaviour
+{
+	private Vector3 startPos;
+	private int dir;
+	private float floatDistance;
+	void Start()
+	{
+		startPos = transform.position;
+		dir = 1;	
+	}
+
+	void Update()
+	{
+
+	if (transform.position.y >= startPos.y + floatDistance)
+		{
+			dir = -1;
+			floatDistance = 0;
+		}
+		if (transform.position.y < startPos.y - floatDistance)
+		{
+			dir = 1;
+			floatDistance = 3;
+		}
+
+		transform.position = new Vector3(transform.position.x,transform.position.y+Time.deltaTime*dir, transform.position.z);
+	}
+}
