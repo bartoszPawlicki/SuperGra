273da8356c716c994bddbf9a59d723a747a4879d
bartol666 <bartekp44@gmail.com>
2018-12-05 20:31:07 +0100

    Add object pool. It's a method to generate objects without the need to instantiate them during actual gameplay, which may be very costly.

---
+++ SuperGra/Assets/Scripts/ObjectPool.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectPool : MonoBehaviour
+{
+    public GameObject pooledObject;
+    private GameObject go;
+
+    public int numberOfObjects;
+    public List<GameObject> pooledObjects;
+
+    private int counter = 0;
+
+    void Start()
+    {
+        pooledObjects = new List<GameObject>();
+
+        for (int i = 0; i < numberOfObjects; i++)
+        {
+            GameObject go = Instantiate(pooledObject);
+            pooledObjects.Add(go);
+            go.SetActive(false);
+        }
+    }
+
+    public GameObject PoolNext(Vector3 position)
+    {
+        int activeCounter = 0;
+        do
+        {
+            go = pooledObjects[counter];
+            counter++;
+            if (counter == numberOfObjects) counter = 0;
+
+            activeCounter++;
+            if (activeCounter == numberOfObjects) throw new UnityException("No remaining pooled objects");
+        }
+        while (go.activeInHierarchy);
+
+        if (go.GetComponent<Rigidbody>() != null) go.GetComponent<Rigidbody>().velocity = Vector3.zero;
+        go.transform.position = position;
+        go.SetActive(true);
+
+        return go;
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
--- SuperGra/Assets/Scripts/Shooting.cs
+++ SuperGra/Assets/Scripts/Shooting.cs
@@ -9,6 +9,9 @@ public class Shooting : MonoBehaviour {
     public GameObject projectile; // pocisk
     public float projectileForce; // sila z jaka wylatuje pocisk z shooting object
 
+    // Korzystamy z poolingu obiektów, żeby nie używać Instantiate w trakcie właściwiej gry.
+    public ObjectPool bulletPool;
+
 	// Use this for initialization
 	void Start () {
 		// tu chyba nic nie potrzebujemy wiec nawet by chyba moznaby to usunac nie?
@@ -27,20 +30,45 @@ public class Shooting : MonoBehaviour {
     void Shoot()
     {
 
-        //projectile instantiation
-        GameObject tempProjectile;
-        tempProjectile = Instantiate(projectile, shootingObject.transform.position, shootingObject.transform.rotation) as GameObject;
+        ////projectile instantiation
+        //GameObject tempProjectile;
+        //tempProjectile = Instantiate(projectile, shootingObject.transform.position, shootingObject.transform.rotation) as GameObject;
 
-        Rigidbody tempRigidBody;
-        tempRigidBody = tempProjectile.GetComponent<Rigidbody>();
+        //Rigidbody tempRigidBody;
+        //tempRigidBody = tempProjectile.GetComponent<Rigidbody>();
+
+        ////nadanie sily pociskowi
+        //tempRigidBody.AddForce(transform.forward * projectileForce);
+
+
+
+        ////zniszczenie pocisku po czasie
+        //Destroy(tempProjectile, 3.0f);
+
+
+
+
+        //alternatywna wersja
+        GameObject tempProjectile = bulletPool.PoolNext(shootingObject.transform.position);
+
+
+        // takie rzeczy można w jednej linijce
+        Rigidbody tempRigidBody = tempProjectile.GetComponent<Rigidbody>();
+
 
         //nadanie sily pociskowi
         tempRigidBody.AddForce(transform.forward * projectileForce);
 
-   
+        // zaczęcie korutnyny. 
+        StartCoroutine(DestroyBullet(tempProjectile));
 
-        //zniszczenie pocisku po czasie
-        Destroy(tempProjectile, 3.0f);
+    }
+
+    //Korutyna jest uruchamiana na osobnym wątku, można też wracać do niej po jakimś czasie - coś jak uśpienie fukncji.
+    public IEnumerator DestroyBullet(GameObject go)
+    {
+        yield return new WaitForSeconds(3f); // po 3 sekundach tutaj wrócimy
+        go.SetActive(false);
 
     }
 }
