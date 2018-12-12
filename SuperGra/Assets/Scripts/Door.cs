75a3ce927b0e9f04f2b41fc085181d8168f8d8bb
precel120 <darkuso@o2.pl>
2018-12-12 22:30:36 +0100

    problem z triggerem i nie chcialo dzialac z ta zmienna remaining enemies

---
+++ SuperGra/Assets/Scripts/Door.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class Door : MonoBehaviour
+{
+    private Vector3 startPos;
+    private float floatDistance;
+    public int dir;
+    public enum DoorState
+    {
+        goingUp,
+        goingDown,
+        stop
+    }
+
+    DoorState doorState;
+    
+	void Start ()
+	{
+	    doorState = DoorState.stop;
+	    startPos = transform.position;
+	}
+	
+	void Update ()
+    {
+        if (doorState == DoorState.goingUp)
+        {
+            // logika na podnoszenie drzwi;
+                if (transform.position.y < startPos.y - floatDistance)
+                {
+                    dir = 1;
+                    floatDistance = 3;
+                }
+
+                if (transform.position.y < 4)
+                {
+                    transform.position = new Vector3(transform.position.x, transform.position.y + Time.deltaTime * dir,
+                        transform.position.z);
+            }
+        }
+        else if (doorState == DoorState.goingDown)
+        {
+            // logika na opuszczanie drzwi;
+            if (transform.position.y >= startPos.y + floatDistance)
+            {
+                dir = -1;
+                floatDistance = 0;
+            }
+            if (transform.position.y >= 1)
+            {
+                transform.position = new Vector3(transform.position.x, transform.position.y + Time.deltaTime * dir,
+                    transform.position.z);
+            } 
+        }
+        else if (doorState == DoorState.stop)
+        {
+            transform.position = new Vector3(transform.position.x, transform.position.y + Time.deltaTime * 0,transform.position.z);
+        }
+    }
+
+    public void ChangeDoorState(DoorState doorState)
+    {
+        this.doorState = doorState;
+    }
+}
---
+++ SuperGra/Assets/Scripts/TrigDoor.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TrigDoor : MonoBehaviour {
+	// Update is called once per frame
+	private void OnTriggerEnter(Collider other)
+	{
+		transform.parent.GetComponent<Door>().ChangeDoorState(Door.DoorState.goingUp);
+	}
+	void OnTriggerExit(Collider other)
+	{
+		// logic about what happens when player enters trigger
+		transform.parent.GetComponent<Door>().ChangeDoorState(Door.DoorState.goingDown);		
+	}
+}
