0676a01aa7aeb46acf78d3f5148da8f85f9b0536
bartoszjasitczak <bartek2707@gmail.com>
2018-12-12 21:45:15 +0100

    Camera and Reload

---
+++ SuperGra/Assets/Scripts/Reloading.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GunScript : MonoBehaviour
+{
+    public int MagazineCapacity;
+    public int ammoInMagazine;
+    public int ammoRemaining;
+    public bool canFire;
+    IEnumerator Reload()
+    {
+        yield return new WaitForSeconds(5);
+        canFire = true;
+    }
+
+    void Start()
+    {
+        canFire = true;
+    }
+
+    void Update()
+    {
+        if (ammoInMagazine <= 0)
+        {
+            if (ammoRemaining >= ammoInMagazine)
+            {
+                StartCoroutine("Reload");
+                if (ammoRemaining >= MagazineCapacity)
+                {
+                    ammoRemaining -= MagazineCapacity;
+                    ammoInMagazine = MagazineCapacity;
+                }
+                else
+                {
+                    ammoInMagazine = ammoRemaining;
+                    ammoRemaining = 0;
+                }
+
+            }
+        }
+
+
+        
+    }
+}
+
+
+
